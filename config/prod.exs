181bea6f7ea983040b1fef46b6e4af7723e95115
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-08-29 04:54:09 -0400

    Adapter Added

--- config/prod.exs
+++ config/prod.exs
@@ -65,6 +65,3 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
 # import_config "prod.secret.exs"
-# Configure your database
-# config :blog_phoenix, BlogPhoenix.Endpoint,
-  
