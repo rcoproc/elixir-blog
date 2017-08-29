ffffbe5c1bd69bfc256081c02c5881db7840edb0
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-08-29 04:58:56 -0400

    Repo Adapter Fixed

--- config/prod.exs
+++ config/prod.exs
@@ -16,11 +16,7 @@ config :blog_phoenix, BlogPhoenix.Endpoint,
   url: [scheme: "https", host: "https://guarded-eyrie-60782.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]], 
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -65,3 +61,8 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
 # import_config "prod.secret.exs"
+config: :blog_phoenix, BlogPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
